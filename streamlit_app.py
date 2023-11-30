c7ebb6b57b6773debbe6fa91a44885ba996f20d3
Lord Affan <124012025+affanlst@users.noreply.github.com>
2023-11-30 19:58:17 +0700

    p

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,15 +20,15 @@ def main_page():
 
 # Membuat sidebar
 sidebar = st.sidebar.selectbox("Navigasi", ("Home", "Car Price","Bar","About"))
-
+# 1LMaL6bqqDw0dHZAFlO-fIBqsVkqazpxb
 def carPrice_page():
-    model = pickle.load(open('model_prediksi_harga_mobil.sav', 'rb'))
+    model = pickle.load(open('https://drive.google.com/uc?export=download&id=1LMaL6bqqDw0dHZAFlO-fIBqsVkqazpxb', 'rb'))
     st.title('Prediksi Harga Mobil')
     st.header("Dataset")
     url = "https://github.com/affanlst/streamlit/blob/main/CP.csv"
     column = ['car_ID','symboling','CarName','fueltype','aspiration','doornumber','carbody','drivewheel','enginelocation','wheelbase','carlength','carwidth','carheight','curbweight','enginetype','cylindernumber','enginesize','fuelsystem','boreratio','stroke','compressionratio','horsepower','peakrpm','citympg','highwaympg','price'
 ]
-    df1 = pd.read_csv('CP.csv', names=column)
+    df1 = pd.read_csv(url, names=column)
     st.dataframe(df1)
 
     st.write("Grafik Highway-mpg")
